2dee9b6b94cde32f28c5731e54edf9d9cdaf22a5
Andres Aguirre <andresgamerpro795@gmail.com>
2025-05-10 20:28:46 -0500

    se crea main

---
+++ Proy_integ_4la/src/main/java/main/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import javax.swing.*;
+import com.mycompany.proy_integ_4la.Login;
+
+public class main { 
+    public static void main(String[] args) {
+        
+       
+            Login login = new Login();  // Instancia correcta
+            login.setVisible(true);    // Mostrar la ventana
+            login.setLocationRelativeTo(null);  // Centrar en pantalla
+     
+    }
+}
